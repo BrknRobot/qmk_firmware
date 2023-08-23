a1468d0ce5100fefdb89765076636d2253316d5a
Broken Robot <byte.northbridge@pm.me>
2023-08-22 19:50:28 -0500

    Restore printscreen

--- keyboards/ergodox_ez/keymaps/npd/keymap.c
+++ keyboards/ergodox_ez/keymaps/npd/keymap.c
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_GRV , KC_A   , KC_O   , KC_E   , KC_U   , KC_I   ,
        TT(SHFT), KC_QUOT, KC_Q   , KC_J   , KC_K   , KC_X   , _______,
        TT(CTRL), KC_LGUI, KC_LALT, KC_LEFT, KC_RGHT,
-                                                    TT(QWERTY), KC_INS ,
+                                                    KC_PSCR, KC_INS ,
                                                              TT(UW),
                                            KC_SPC , KC_TAB , TT(MDIA),
        // right hand
@@ -87,8 +87,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [QWERTY] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,           KC_MINS,
-  KC_DEL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    _______,              _______,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,           KC_BSLS,
+  KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
+  KC_DEL,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    XXXXXXX,              XXXXXXX,      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   KC_BSPC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),         MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   _______, _______, _______, _______, _______,                                                       _______, _______, _______, _______, _______,
@@ -150,7 +150,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        _______, _______, _______, _______, _______,
                                                     _______, _______,
                                                              _______,
-                                           _______, _______, _______,
+                                           TG(QWERTY), _______, _______,
        // right hand
        _______, _______, _______, _______, _______, _______, _______,
        _______, _______, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______,
